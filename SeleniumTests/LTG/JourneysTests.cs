ac73527b6b2cbed065326cdd3dfb802a973d94c0
Gabrielius Marozas <marozasgabrielius@gmail.com>
2023-03-07 10:34:12 +0200

    Modified code for checking

--- SeleniumTests/LTG/JourneysTests.cs
+++ SeleniumTests/LTG/JourneysTests.cs
@@ -21,12 +21,11 @@ namespace SeleniumTests.LTG
 
             Journeys.ClickCookieButton();
             Journeys.LeavingRouteAndClass();
-            Journeys.LeavingRouteAndClass();
-            //Journeys.ClickAddToBooking();
-            //Journeys.ArrivalRouteAndClass();
-            //Journeys.ClickAddToBooking();
-            //Journeys.ClickContinueForSeatSelection();
-            //Journeys.ClickContinueForPublicTransport();
+            Journeys.ClickAddToBooking();
+            Journeys.ArrivalRouteAndClass();
+            Journeys.ClickAddToBooking();
+            Journeys.ClickContinueForSeatSelection();
+            Journeys.ClickContinueForPublicTransport(); // ties čia fail'ina. OpenQA.Selenium.StaleElementReferenceException : stale element reference: element is not attached to the page document
             //Journeys.ClickContinueToCheckout();
             //Journeys.InputEmail(valueEmail);
             //Journeys.InputPhoneNumber(valueNumber);
