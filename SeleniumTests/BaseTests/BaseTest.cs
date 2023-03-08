a6edc9e6eb0054d1391f9364d7a503a0d0503525
Gabrielius Marozas <marozasgabrielius@gmail.com>
2023-03-08 07:57:51 +0200

    Modify to create a screenshot only when test does not succeed

--- SeleniumTests/BaseTests/BaseTest.cs
+++ SeleniumTests/BaseTests/BaseTest.cs
@@ -1,5 +1,6 @@
 ﻿using Framework;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace SeleniumTests.BaseTests
 {
@@ -13,7 +14,11 @@ namespace SeleniumTests.BaseTests
         [TearDown]
         public void Teardown()
         {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
             Driver.TakeScreenShot();
+            }
+
             Driver.QuitDriver();
         }
     }
