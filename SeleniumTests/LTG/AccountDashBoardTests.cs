d793c7dde148b9ed8be212e773da32598cbd43de
Gabrielius Marozas <marozasgabrielius@gmail.com>
2023-03-09 11:19:00 +0200

    Add SigInForm() in AccountDashboardTests

---
+++ SeleniumTests/LTG/AccountDashBoardTests.cs
@@ -0,0 +1,48 @@
+﻿using Framework;
+using Framework.Pages.LTG;
+using NUnit.Framework;
+using SeleniumTests.BaseTests;
+
+namespace SeleniumTests.LTG
+{
+    internal class AccountDashBoardTests : BaseTestSingleSession
+    {
+        [Test, Order(1)]
+        public void SignInForm()
+        {
+            string valueUserName = "lahad98234@v2ssr.com";
+            string valuePassword = "Tester.123";
+            string expectedText = "Bilietai";
+
+            LTGaccount.CookieButtonClick();
+            LTGaccount.PressHamburegerButton();
+            LTGaccount.PressSignInButton();
+            LTGaccount.UserNameInput(valueUserName);
+            LTGaccount.PasswordInput(valuePassword);
+            LTGaccount.ClickConfirmButton();
+
+
+            Assert.AreEqual(expectedText, LTGaccount.SignInResultOutput());
+        }
+
+        [Test, Order(2)]
+        public void EditingPhoneNumberToValidAndInvalidForm()
+        {
+            
+            string valueValidPhoneNumber = "68416087";
+            string valueInvalidPhoneNumber = "Invalid";
+            string expectedText = "Telefono numeryje galimi tik skaičiai, tarpai ir - arba + simboliai.";
+
+            AccountDashBoard.ClickAccountDetails();
+            AccountDashBoard.ClickEditDetails();
+            AccountDashBoard.InputValidPhoneNumber(valueValidPhoneNumber);
+            AccountDashBoard.ClickSaveButton();;
+
+            AccountDashBoard.ClickEditDetails();
+            AccountDashBoard.InputValidPhoneNumber(valueInvalidPhoneNumber);
+            AccountDashBoard.ClickSaveButton();
+
+            Assert.AreEqual(expectedText, AccountDashBoard.UnsuccessfulUpdate());
+        }
+    }
+}
