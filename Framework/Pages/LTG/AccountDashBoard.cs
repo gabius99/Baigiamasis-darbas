e627fb80065a9a7016992f3ec66469d5fa0f9804
Gabrielius Marozas <marozasgabrielius@gmail.com>
2023-03-09 11:33:03 +0200

    Removed unnnecessasry information

---
+++ Framework/Pages/LTG/AccountDashBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Framework.Pages.LTG
+{
+    public class AccountDashBoard
+    {
+        public static void ClickAccountDetails()
+        {
+            Common.WaitForElementToBeEnabled(Locators.AccountDashBoard.ButtonAccountDetails);
+            Common.ClickElement(Locators.AccountDashBoard.ButtonAccountDetails);
+        }
+
+        public static void ClickEditDetails()
+        {
+            Common.WaitForElementToBeInvisible(Locators.AccountDashBoard.Loader);
+            Common.ScrollUntilElementIsClickable(Locators.AccountDashBoard.ButtonEditDetails);
+        }
+
+        public static void ClickSaveButton()
+        {
+            Common.ScrollUntilElementIsClickable(Locators.AccountDashBoard.ButtonSave);
+        }
+
+        public static void InputValidPhoneNumber(string valueValidPhoneNumber)
+        {
+            Common.WaitForElementToBeEnabled(Locators.AccountDashBoard.PhoneNumber);
+            Common.DoubleClickElement(Locators.AccountDashBoard.PhoneNumber);
+            Common.SendKeys(Locators.AccountDashBoard.PhoneNumber, valueValidPhoneNumber);
+        }
+
+        public static object UnsuccessfulUpdate()
+        {
+            return Common.GetElementText(Locators.AccountDashBoard.UnsuccessNotification);
+        }
+    }
+}
