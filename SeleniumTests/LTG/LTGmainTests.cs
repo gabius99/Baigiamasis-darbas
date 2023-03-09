bbe08cac00501dd2b9b18433b682f2a1136996f6
Gabrielius Marozas <marozasgabrielius@gmail.com>
2023-03-09 11:19:54 +0200

    Add ReservingSeatsDemo in LTGmainTests

--- SeleniumTests/LTG/LTGmainTests.cs
+++ SeleniumTests/LTG/LTGmainTests.cs
@@ -5,9 +5,9 @@ using SeleniumTests.BaseTests;
 
 namespace SeleniumTests.LTG
 {
-    internal class LTGmainTests : BaseTest
+    internal class LTGmainTests : BaseTestSingleSession
     {
-        [Test]
+        [Test, Order(1)]
         public void CheckForTicketSearch()
         {
             string expectedText = "Paieškos rezultatai";
@@ -27,6 +27,29 @@ namespace SeleniumTests.LTG
             LTGmain.SearchButton();
 
             Assert.AreEqual(expectedText, LTGmain.SearchResultsOutput());
+        }
+
+
+        [Test, Order(2)]
+        public void ReservingSeatsDemo()
+        {
+            string valueEmail = "lahad98234@v2ssr.com";
+            string valueNumber = "68462879";
+            string expectedResults = "MOKĖTI IŠ BANKO SĄSKAITOS";
+
+            Journeys.LeavingRouteAndClass();
+            Journeys.ClickAddToBooking();
+            Journeys.ArrivalRouteAndClass();
+            Journeys.ClickAddToBooking();
+            Journeys.ClickContinueForSeatSelection();
+            Journeys.ClickContinueForPublicTransport();
+            Journeys.ClickContinueToCheckout();
+            Journeys.InputEmail(valueEmail);
+            Journeys.InputPhoneNumber(valueNumber);
+            Journeys.ClickOnCheckBox();
+            Journeys.ClickReserveYourSeat();
+
+            Assert.AreEqual(expectedResults, Journeys.PaymentPageTitle());
         }
     }
 }
