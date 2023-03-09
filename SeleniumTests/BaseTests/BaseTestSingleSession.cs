7dc4f611695577a27b73b1429fd184c6f2f7075b
Gabrielius Marozas <marozasgabrielius@gmail.com>
2023-03-09 11:16:22 +0200

    Add new BaseTest fo single sessions

---
+++ SeleniumTests/BaseTests/BaseTestSingleSession.cs
@@ -0,0 +1,28 @@
+﻿using Framework;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace SeleniumTests.BaseTests
+{
+    internal class BaseTestSingleSession
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Driver.SetupDriver();
+            Driver.OpenUrl("https://ltglink.lt/");
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                string fileName = Driver.TakeScreenShot(TestContext.CurrentContext.Test.MethodName);
+                TestContext.AddTestAttachment(fileName);
+            }
+
+            //Driver.QuitDriver();
+        }
+    }
+}
